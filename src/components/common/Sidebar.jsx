d4c2c40b262b0c8dd1b81f5c3f3c34d8727a4980
devhg99 <et3015@naver.com>
2025-09-12 14:31:54 +0900

    sidebar-v2

--- src/components/common/Sidebar.jsx
+++ src/components/common/Sidebar.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { NavLink, useNavigate } from "react-router-dom";
+import React, { useState, useEffect } from "react"; 
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
 
 /** 경로 매핑 */
 export const PATHS = { 
